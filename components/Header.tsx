f323d8cea4609e2059759e8b037d91491c08b6da
Peter Xu <sysu.peter.hsu@gmail.com>
2024-04-01 23:07:21 +0800

    feat: update logo and personal info

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,7 @@
+import Image from 'next/image'
 import siteMetadata from '@/data/siteMetadata'
 import headerNavLinks from '@/data/headerNavLinks'
-import Logo from '@/data/logo.svg'
+import Logo2 from '@/data/logo.png'
 import Link from './Link'
 import MobileNav from './MobileNav'
 import ThemeSwitch from './ThemeSwitch'
@@ -13,7 +14,7 @@ const Header = () => {
         <Link href="/" aria-label={siteMetadata.headerTitle}>
           <div className="flex items-center justify-between">
             <div className="mr-3">
-              <Logo />
+              <Image height={40} alt="" src={Logo2} />
             </div>
             {typeof siteMetadata.headerTitle === 'string' ? (
               <div className="hidden h-6 text-2xl font-semibold sm:block">
