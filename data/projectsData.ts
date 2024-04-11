c85e1d81ff1b18b481b2f7d756a4aeb9c89571cb
xupea <xupea@192.168.0.107>
2024-04-11 22:54:28 +0800

    feat: update project

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -7,20 +7,16 @@ interface Project {
 
 const projectsData: Project[] = [
   {
-    title: 'A Search Engine',
-    description: `What if you could look up any information in the world? Webpages, images, videos
-    and more. Google has many features to help you find exactly what you're looking
-    for.`,
+    title: 'Electron DevTools Installer',
+    description: `Electron 应用中调试React Vue Redux 插件`,
     imgSrc: '/static/images/google.png',
-    href: 'https://www.google.com',
+    href: 'https://www.npmjs.com/package/electron-devtools-assembler',
   },
   {
-    title: 'The Time Machine',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten headphones with this simple yet affordable solution.`,
+    title: 'Stateful Electron-window',
+    description: `有记忆的Electron主窗口`,
     imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
+    href: 'https://www.npmjs.com/package/stateful-electron-window',
   },
 ]
 
